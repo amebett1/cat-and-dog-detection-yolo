958c3fe541e631a69e01cd67fdd9414d4bf8cb23
Đồng Minh Đức <dongminhduc1903@gmail.com>
2026-01-19 10:03:29 +0700

    update model

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO('runs/detect/animal_detector_v10/weights/best.pt')
+model = YOLO('runs/detect/train/weights/best.pt')
 
-
-model.predict(source='1115.mp4', save=False, conf=0.5, show=True)
+model.predict(source='images.jpg', save=True, conf=0.5, show=True)
--- train.py
+++ train.py
@@ -1,12 +1,19 @@
 import os
+
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-    
-    config_path = './config.yaml'
 
+
+if __name__ == '__main__':
+    config_path = './config.yaml'
     # Load model
     model = YOLO('yolov8n.pt')
-
     # Train model
-    model.train(data=config_path, epochs=200, batch=32, name='animal_detector_v8')
+    model.train(
+        data='./config.yaml', 
+        epochs=200, 
+        batch=32,
+        imgsz=640, 
+        device=0,
+        workers=0
+    )
